7fbddbd94540500f01dfb63d7ee3510d1027def0
gm <geramoralesrico@gmail.com>
2021-05-10 17:27:47 -0500

    Last: Started admin models to customise admin interface

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,26 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+#Change how Models display in admin interface
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author,AuthorAdmin)
+
+ 
+@admin.register(Book)   #same as "admin.site.register"
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)   #same as "admin.site.register"
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
--- catalog/models.py
+++ catalog/models.py
@@ -34,6 +34,11 @@ class Book(models.Model):
     def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
 
+    def display_genre(self):
+        '''Func to display genre in admin'''
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'
+
 class BookInstance(models.Model):
     '''represents a specific copy of a book (that can be borrowed)'''
     id = models.UUIDField(primary_key=True,default=uuid.uuid4,help_text='ID for this particular book across library')
