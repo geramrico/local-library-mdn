f12834e4beabbb41e65cae94b202b8641e867dd9
Gerardo Morales <geramoralesrico@gmail.com>
2021-05-16 00:20:14 -0500

    Created URL for "index" and view func for index

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    # Function path defines:
+    #     1. URL pattern: empty string
+    #     2. View function thatll be called if the URL pattern is detected
+    #     3. Name: unique identifier for this URL mapping
+    path('', views.index, name='index'),
 ]
