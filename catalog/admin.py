051f30aaabff38db5a48bb813b34f6684cdac419
Gerardo Morales <geramoralesrico@gmail.com>
2021-05-15 22:36:54 -0500

    Created/modified admin panel views

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,7 +3,7 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-# admin.site.register(Book)
+# admin.site.register(Book), se quita porque se agrego abajo con el decorador @admn.register
 # admin.site.register(Author)
 # admin.site.register(BookInstance)
 admin.site.register(Genre)
@@ -12,17 +12,47 @@ admin.site.register(Language)
 
 #Change how Models display in admin interface
 
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+#Edicion "en linea" de registros asociados xxx
+class BookInline(admin.TabularInline):
+    model = Book
 
-admin.site.register(Author,AuthorAdmin)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    #Mostar en tabla en panel de admin
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    #Para cambiar como se visualiza un campo de info en la vista de editar   
+    fields = ['first_name','last_name',('date_of_birth','date_of_death')]
+    inlines = [BookInline]
+
+#Edicion "en linea" de registros asociados xxx
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 
  
 @admin.register(Book)   #same as "admin.site.register"
 class BookAdmin(admin.ModelAdmin):
+    #Mostrar en panel de admin
     list_display = ('title', 'author', 'display_genre')
+
+    #Edicion "en linea" de registros asociados xxx
+    inlines = [BookInstanceInline]
+
 
 @admin.register(BookInstance)   #same as "admin.site.register"
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+     #Agrega la opcion de filtrar para buscar
+    list_filter = ('status','due_back') 
 
+    #Agrupa en el formulario de BookInstance
+    fieldsets = (
+    (None, {
+            'fields': ('book', 'imprint', 'id')
+    }),
+    ('Availability', {
+        'fields': ('status', 'due_back')
+    }),
+    )
+
+    list_display = ('book', 'status','due_back','id')
