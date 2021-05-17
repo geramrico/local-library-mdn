0ddc9d2e92db22f917836f295ee17a3d778e0f3a
Gerardo Morales <geramoralesrico@gmail.com>
2021-05-16 20:49:17 -0500

    Up to: tutorial pt6. generic views and detail views

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,19 @@ urlpatterns = [
     #     2. View function thatll be called if the URL pattern is detected
     #     3. Name: unique identifier for this URL mapping
     path('', views.index, name='index'),
+    
+
+    # URLS for Books
+    # URL for view implemented as a class
+    path('books/', views.BookListView.as_view(), name='books'),
+
+    #URL Map for book detail pages
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+    # URLS for Authors
+    # URL for view implemented as a class
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+
+    #URL Map for author detail pages
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book,Author,BookInstance,Genre
+from django.views import generic
 
 # Create your views here.
 
@@ -32,3 +33,38 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(request, primary_key):
+        try:
+            book = Book.objects.get(pk=primary_key)
+        except Book.DoesNotExist:
+            raise Http404('Book does not exist')
+        
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+### LAST -> AGREGE ESTO 
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def author_detail_view(request, primary_key):
+        try:
+            author = Author.objects.get(pk=primary_key)
+        except Author.DoesNotExist:
+            raise Http404('Book does not exist')
+        
+        return render(request, 'catalog/author_detail.html', context={'author': author})
