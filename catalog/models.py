6ee9158095995070c50f471ba731f9f771fb296d
Gerardo Morales <geramoralesrico@gmail.com>
2021-05-19 23:14:35 -0500

    Start 'librarian-view' challenge

--- catalog/models.py
+++ catalog/models.py
@@ -65,6 +65,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     '''representation of the object in human readable form'''
     def __str__(self):
